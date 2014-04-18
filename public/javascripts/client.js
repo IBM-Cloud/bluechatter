888d832b401b3c4ece5fe0d7cecba91a25c1f0c5
Ryan J Baxter <rbaxter85@apache.org>
2014-04-18 17:32:47 -0400

    Fixed spacing

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -15,7 +15,7 @@
 //------------------------------------------------------------------------------
 
 $(document).ready(function() {
-	var name = '';
+  var name = '';
 
   $.getJSON('/instanceId', function(response, statusText, jqXHR) {
     if(jqXHR.status == 200) {
@@ -36,12 +36,12 @@ $(document).ready(function() {
       go();
     }
   });
-	$('.go-user').on('click', function(e) {
-		go();
-	});
+  $('.go-user').on('click', function(e) {
+    go();
+  });
 
-	$('.chat-box textarea').keydown(function(e) {
-	  if(e.keyCode == 13){
+  $('.chat-box textarea').keydown(function(e) {
+    if(e.keyCode == 13){
       $.ajax({
         type: "POST",
         url: "/msg",
@@ -52,19 +52,19 @@ $(document).ready(function() {
       $('.jumbotron').hide();
       e.preventDefault()
     }
-	});
+  });
 
-	function poll() {
-	  $.getJSON('/poll/' + new Date().getTime(), function(response, statusText, jqXHR) {
+  function poll() {
+    $.getJSON('/poll/' + new Date().getTime(), function(response, statusText, jqXHR) {
       if(jqXHR.status == 200) {
         $('.jumbotron').hide();
         msg = response;
         var html = '<div class="panel panel-success"><div class="panel-heading"><h3 class="panel-title">' + msg.username + '</h3></div><div class="panel-body">' + msg.message + '</div></div>';
         $('.message-area').append(html);
         var d = $('.message-area');
-		    d.scrollTop(d.prop("scrollHeight"));
+        d.scrollTop(d.prop("scrollHeight"));
       }
       poll();
     });
-	};
+  };
 });
